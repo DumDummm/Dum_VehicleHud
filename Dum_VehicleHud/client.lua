f9150ea34e334c386dcc63552c2a50f29abe8f01
DumDummm <85816814+DumDummm@users.noreply.github.com>
2021-06-13 06:37:20 +0200

    Add files via upload

---
+++ Dum_VehicleHud/client.lua
@@ -0,0 +1,30 @@
+--Miles / Hour
+local mph = 2.2369
+-- Kilometers / Hour
+local kmph = 3.6
+
+--Display Text Subroutine
+function showText(text)
+    SetTextFont(4)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(0.02,0.7) --{x,y}
+end
+
+--Speedmeter Thread
+Citizen.CreateThread(function()
+while true do
+        Citizen.wait(1) --Adds A Small Delay to Avoid Crahing
+        local speed = (GetEntitySpeed(GetVehicleIsPedIsIn(GetPlayerPed(-1, false))*mph)
+        if (isPedInAnyVehicle(GetPlayerPed(-1, false)) then
+            if speed < 1 then
+                showText("")
+            else
+                showText(math.floor(speed))
+            end
+        end
+    end
+end)
---
+++ Dum_VehicleHud/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+auther 'Dum Dum'
+description ' A vehicle Hud'
+version '1.0.0'
+
+client_script 'client.lua'
